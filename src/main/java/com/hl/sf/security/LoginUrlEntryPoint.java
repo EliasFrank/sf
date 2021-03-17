09fcb0e798fad63c5f07169b357c53545e898ebd
elias <elias_git@qq.com>
2021-03-17 11:05:59 +0800

    完结撒花

--- src/main/java/com/hl/sf/security/LoginUrlEntryPoint.java
+++ src/main/java/com/hl/sf/security/LoginUrlEntryPoint.java
@@ -5,12 +5,18 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 import org.springframework.util.AntPathMatcher;
 import org.springframework.util.PathMatcher;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginUrlEntryPoint extends LoginUrlAuthenticationEntryPoint {
+    private static final String API_FREFIX = "/api";
+    private static final String API_CODE_403 = "{\"code\": 403}";
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
 
     private PathMatcher pathMatcher = new AntPathMatcher();
     private final Map<String, String> authEntityPointMap;
@@ -32,5 +38,21 @@ public class LoginUrlEntryPoint extends LoginUrlAuthenticationEntryPoint {
             }
         }
         return super.determineUrlToUseForThisRequest(request, response, exception);
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        String uri = request.getRequestURI();
+        if (uri.startsWith(API_FREFIX)) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(CONTENT_TYPE);
+
+            PrintWriter pw = response.getWriter();
+            pw.write(API_CODE_403);
+            pw.close();
+        } else {
+            super.commence(request, response, authException);
+        }
+
     }
 }
